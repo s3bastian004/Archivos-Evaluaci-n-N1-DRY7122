68c4d17732028c9d1f6fc34a1a791a23e0eb8b77
NetAcad DEVASC <changeme@example.com>
2023-04-05 00:03:13 +0000

    Evaluación N°1 DRY7122

---
+++ prueba1.py
@@ -0,0 +1,6 @@
+####
+print ("Evaluación N1 Programación y Redes Virtualzidas")
+print ("Sebastian Ibaceta")
+print ("Gabriel Vasquez")
+
+
---
+++ prueba1file2.py
@@ -0,0 +1,7 @@
+##########################################
+firstName = input ("¿Cual es tu nombre? ")
+lastname = input ("¿Cual es tu apellido? ")
+sección = input ("¿Cual es tu sección? ")
+sede = input ("¿Cual es tu sede?")
+print("Hola " + firstName, lastname + "! Tu sección es "+ sección +" y tu sede es "+ sede +" .")
+
